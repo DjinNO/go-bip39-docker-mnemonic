80368ded14cc0845adcc8d5869ba1b662a51ea6f
Artyom Zverev <artisst03@gmail.com>
2022-04-16 16:47:35 +0300

    test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# go-bip39-docker-mnemonic
+docker pull djinno/go-bip39-docker-mnemonic 12
+or
+docker pull djinno/go-bip39-docker-mnemonic 24
+for 24 mnemonic
--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/tyler-smith/go-bip39"
 )
 
 func main() {
-	entropy, _ := bip39.NewEntropy(256)
-	mnemonic, _ := bip39.NewMnemonic(entropy)
-	fmt.Println(mnemonic)
+	if os.Args[1] == "12" {
+		entropy, _ := bip39.NewEntropy(128)
+		mnemonic, _ := bip39.NewMnemonic(entropy)
+		fmt.Println(mnemonic)
+	} else {
+		entropy, _ := bip39.NewEntropy(256)
+		mnemonic, _ := bip39.NewMnemonic(entropy)
+		fmt.Println(mnemonic)
+	}
+
 }
